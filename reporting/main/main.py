ae621bd79aba2c9c87cd12da583fb0b2e01ac65d
Piotr Kopalko <copalco@gmail.com>
2017-09-14 17:21:29 +0200

    Add wsgi function

--- reporting/main/main.py
+++ reporting/main/main.py
@@ -1,9 +1,14 @@
 import knot
 
-from . import wsgi
+from reporting.main import wsgi
 
 
 def main():
     container = knot.Container()
     wsgi.setup(container)
     return container
+
+
+def create_app(env, start_response):
+    container = main()
+    return container('web.wsgi.app')(env, start_response)
